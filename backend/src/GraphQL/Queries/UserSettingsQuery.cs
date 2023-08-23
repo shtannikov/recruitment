63ef2068e4a740c4d7e9899f3e32b9c04ec1c44f
shtannikov <e.shtannikov@gmail.com>
2023-08-23 13:41:10 +0600

    adding nullability checks for `user identity`

--- backend/src/GraphQL/Queries/UserSettingsQuery.cs
+++ backend/src/GraphQL/Queries/UserSettingsQuery.cs
@@ -9,10 +9,14 @@ public class UserSettingsQuery
         [Service] IHttpContextAccessor httpContextAccessor,
         [Service] UserManager<ApplicationUser> userManager)
     {
-        var username = httpContextAccessor.HttpContext!
-            .User
-            .Identity!
-            .Name!;
+        var username = httpContextAccessor.HttpContext
+            ?.User
+            .Identity
+            ?.Name;
+
+        if (username is null)
+            throw new InvalidOperationException("Can not get user identity");
+
         var user = await userManager.FindByNameAsync(username);
         var userRoles = await userManager.GetRolesAsync(user!);
         return Enum.GetValues<UserRole>()
